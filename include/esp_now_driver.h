31e3962c28970dfd95e489178a320cf97e76bcbf
Asmit-Singh-01 <asmitsingh044@gmail.com>
2026-08-27 15:41:28 +0530

    Add EspNowDriver class for ESP-NOW communication

---
+++ include/esp_now_driver.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "radio_driver.h"
+
+class EspNowDriver : public IRadioDriver {
+private:
+    static void (*app_rx_callback)(const uint8_t* src_mac, const uint8_t* data, size_t len, int8_t rssi);
+
+public:
+    EspNowDriver() = default;
+    ~EspNowDriver() override = default;
+
+    bool init() override;
+    bool send_bytes(const uint8_t* target_mac, const uint8_t* data, size_t len) override;
+    void set_rx_callback(void (*callback)(const uint8_t* src_mac, const uint8_t* data, size_t len, int8_t rssi)) override;
+    
+    static void handle_raw_receive(const uint8_t* mac_addr, const uint8_t* incoming_data, int len, int8_t rssi);
+};
