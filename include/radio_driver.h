fe83955d46af380c4edcf154a77914ada7aca66a
Asmit Singh <asmitsingh044@gmail.com>
2026-09-13 08:43:50 +0530

    refactor: Upgrade radio_driver.h interface with modern C++ types and callback alias

--- include/radio_driver.h
+++ include/radio_driver.h
@@ -1,12 +1,15 @@
 #pragma once
-#include <stdint.h>
-#include <stddef.h>
+
+#include <cstdint>
+#include <cstddef>
 
 class IRadioDriver {
 public:
+    using RxCallback = void (*)(const uint8_t* src_mac, const uint8_t* data, size_t len, int8_t rssi);
+
     virtual ~IRadioDriver() = default;
     
     virtual bool init() = 0;
     virtual bool send_bytes(const uint8_t* target_mac, const uint8_t* data, size_t len) = 0;
-    virtual void set_rx_callback(void (*callback)(const uint8_t* src_mac, const uint8_t* data, size_t len, int8_t rssi)) = 0;
+    virtual void set_rx_callback(RxCallback callback) = 0;
 };
