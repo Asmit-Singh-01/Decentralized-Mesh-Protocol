1a6b0d6955fa7dd4d1f6a3eea3a9b943d9678203
Asmit Singh <asmitsingh044@gmail.com>
2026-09-13 08:39:51 +0530

    refactor: Update MeshNode header to match cpp signatures and use unordered_set

--- include/mesh_node.h
+++ include/mesh_node.h
@@ -1,8 +1,10 @@
 #pragma once
+
 #include "packet_format.h"
-#include <vector>
+#include <cstdint>
+#include <cstddef>
 #include <unordered_map>
-#include <stdint.h>
+#include <unordered_set>
 
 struct PeerInfo {
     uint16_t node_id;
@@ -16,16 +18,16 @@ private:
     uint16_t node_id;
     uint16_t current_seq;
     std::unordered_map<uint16_t, PeerInfo> routing_table;
-    std::vector<uint16_t> seen_packets;
+    std::unordered_set<uint16_t> seen_packets;
 
     bool is_duplicate(uint16_t seq);
-    void update_peer(uint16_t sender_id, int8_t rssi, uint8_t hops);
+    void update_peer(uint16_t sender_id, int8_t rssi, uint8_t hops, uint32_t current_time_ms);
 
 public:
     MeshNode(uint16_t id);
     
     void init();
-    void handle_received_packet(const uint8_t* raw_data, size_t len, int8_t rssi);
+    void handle_received_packet(const uint8_t* raw_data, size_t len, int8_t rssi, uint32_t current_time_ms = 0);
     bool broadcast_payload(PacketType type, const uint8_t* data, uint8_t len);
     bool send_to_node(uint16_t target_id, PacketType type, const uint8_t* data, uint8_t len);
     
